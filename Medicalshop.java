3c11cb9c3f21c68523424aa98e7caea72ca021f7
sohailxworkz <sohailzehen1909@gmail.com>
2024-07-05 09:41:01 +0530

    METHODS Basics Program

---
+++ Medicalshop.java
@@ -0,0 +1,12 @@
+class Medicalshop }
+
+ // Datatypes ref = value ;
+ static String name = "Sri venkateshwara Chemist & Druigst";
+ static String tableNames[]={"Dolowin Plus , "Haloset" ,Okacel" };
+  public static void main(String medi[])}
+  System.out.println("Main stated")
+  
+  System.out.println("Available Tablets/Medicine Are " + tablets[0]+" ,"+
+  tablets[1] + " +tablets[2]+ " "tablets[3]);
+  System.out.println("Main ended");
+ {
---
+++ subtraction.java
@@ -0,0 +1,15 @@
+class Subtraction{
+   public static void main{String [] subtraction}{
+   System.out.println{"Main started"};
+   sub (97,39);
+   sub (297,112);
+   System.out.println("Main ended");
+   }
+   
+   public static void sub(int num1 , int num2){
+	   
+    System.out.println("sub method started");
+	   System.out.println(num1-num2);
+	   System.out.println("sub method ended");
+	   return;
+	   }
